ad4e7b263db549ea61ba90fa543216cb6094c5db
Bram Cohn <BramCohn@gmail.com>
2025-06-09 22:20:38 -0600

    All tests passing

--- api/tasks.js
+++ api/tasks.js
@@ -4,17 +4,55 @@ export default router;
 
 import requireBody from "#middleware/requireBody";
 import requireUser from "#middleware/requireUser";
-import { createTask } from "#db/queries/tasks";
+import {
+  createTask,
+  getTasksByUserId,
+  updateTask,
+  getTaskById,
+  deleteTask,
+} from "#db/queries/tasks";
 
 router.use(requireUser);
 
-router.route("/").post(requireBody(["title", "done"]), async (req, res) => {
-  const { title, done } = req.body;
-  const newTask = await createTask(title, done, req.user.id);
-  res.status(201).send(newTask);
-});
+router
+  .route("/")
+  .post(requireBody(["title", "done"]), async (req, res) => {
+    const { title, done } = req.body;
+    const newTask = await createTask(title, done, req.user.id);
+    res.status(201).send(newTask);
+  })
+  .get(async (req, res) => {
+    const tasks = await getTasksByUserId(req.user.id);
+    res.status(200).send(tasks);
+  });
 
 router.param("id", async (req, res, next, id) => {
   const task = await getTaskById(id);
   if (!task) return res.status(404).send("Task not found");
+  if (task.user_id !== req.user.id) {
+    return res.status(403).send("This is not your task");
+  }
+  req.task = task;
+  next();
 });
+
+router
+  .route("/:id")
+  .put(requireBody(["title", "done"]), async (req, res) => {
+    const { title, done } = req.body;
+    const updatedTask = await updateTask(
+      req.params.id,
+      title,
+      done,
+      req.user.id
+    );
+    if (!updatedTask)
+      return res.status(403).json({ message: "You do not own this task" });
+    res.status(200).send(updatedTask);
+  })
+  .delete(async (req, res) => {
+    const deletedTask = await deleteTask(req.task.id);
+    if (req.user.id != req.task.user_id)
+      return res.status(403).json({ message: "You do not own this task" });
+    res.status(204).send(deletedTask);
+  });
--- db/queries/tasks.js
+++ db/queries/tasks.js
@@ -7,3 +7,33 @@ export async function createTask(title, done, userId) {
   } = await db.query(sql, [title, done, userId]);
   return task;
 }
+
+export async function getTaskById(id) {
+  const sql = `SELECT * FROM tasks WHERE id = $1`;
+  const {
+    rows: [task],
+  } = await db.query(sql, [id]);
+  return task;
+}
+
+export async function getTasksByUserId(id) {
+  const sql = `SELECT * FROM tasks WHERE user_id = $1`;
+  const { rows: task } = await db.query(sql, [id]);
+  return task;
+}
+
+export async function updateTask(id, title, done, ownerId) {
+  const sql = `UPDATE tasks SET title = $2, done = $3 WHERE id = $1 AND user_id = $4 RETURNING *`;
+  const {
+    rows: [updatedTask],
+  } = await db.query(sql, [id, title, done, ownerId]);
+  return updatedTask;
+}
+
+export async function deleteTask(id) {
+  const sql = `DELETE FROM tasks WHERE id = $1 RETURNING *`;
+  const {
+    rows: [task],
+  } = await db.query(sql, [id]);
+  return task;
+}
